0adbfe9ef79b4cdd3d9192bdfb78b4ce2eab9dd1
olivererdmann <o.erdmann@finaldream.de>
2015-09-24 15:48:19 +0200

    Implements Commander-interface and adds option `--prefix`.
    * help-functionality is provided by commander.
    * `--prefix` allows adding prefixes to svg-selectors.

--- index.js
+++ index.js
@@ -3,8 +3,29 @@
 'use strict';
 
 var Svg2CssApp = require('./lib/svg2css.js');
+var cli        = require('commander');
+var pJson      = require('./package.json');
 
 process.title = 'svg2css';
 
+var desc = [
+    'Svg2Css Utility, Version ' + pJson.version,
+    '',
+    'Takes a folder of SVG-files and translates them into a single CSS-file with inline background-images.',
+    'The filenames will be used as CSS-selectors for the generated rules.'
+].join('\n');
+
+
+cli.version(pJson.version)
+    .description(desc)
+    .usage('[options] <source-dir> <target-file>')
+    .option('--prefix [value]', 'Prepended string to each selector in the resulting css.')
+    .parse(process.argv);
+
+if (!process.argv.slice(2).length) {
+    cli.outputHelp();
+    return;
+}
+
 var app = new Svg2CssApp();
-app.init(process.argv.slice(2));
+app.init(cli);
